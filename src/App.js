b4bcfa77f7bc0bc4eb24ab77215612c779cda914
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-09-22 14:23:50 -0500

    take the wheel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+class FetchBeers extends Component {
+  constructor(props){
+    super(props);
+  state = {
+    beerList: []
+  }
+}
+
+componentDidMount(){
+  this.fetchData();
+ }
+}
+
+fetchData(){
+
+  fetch('https://api.punkapi.com/v2/beers')
+  .then(response => response.json())
+  .then(parsedJson => console.log(parsedJson.results))
+  .catch(error = console.log('parsing failed', error))
+}
+
 
 function App() {
   return (
