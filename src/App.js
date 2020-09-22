91eed51a76d0419eaaf6362851829b819c391f50
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-09-22 13:47:11 -0500

    words

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PUNK API BABY
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <button>Gimme Beer</button>
       </header>
     </div>
   );
